525b457d8a74b3c6ee00db70e5fa64658ec4c274
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2019-11-16 19:37:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '07be8a7e1bb173fe0c1bf2092a8bedf59902b256c49bb53892fe1555ffa76fc1a3e9967ab1033e28c6e23'
-group_id = '182028902'
+token = ''
+group_id = ''
 
 start_message = ['Меню', 'меню', 'start', 'Start', 'Старт', 'старт']
